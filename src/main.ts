902a9709d95fddd30e68029a2ab22424d04b5f05
Nikolay Yakimov <root@livid.pp.ru>
2019-03-06 16:44:03 +0300

    A couple config options

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,17 @@ export function provideUPI(): UPI.IRegistrationOptions {
       },
     },
     events: {
-      onDidSaveBuffer: (buf) => checkFile(buf, true) as any,
+      onDidSaveBuffer: (buf) =>
+        checkFile(
+          buf,
+          atom.config.get('ide-haskell-hlint').checkAllFilesInProject
+            ? 'dir'
+            : 'file',
+        ) as any,
+      onDidStopChanging: (buf) => {
+        if (atom.config.get('ide-haskell-hlint').checkOnChange)
+          return checkFile(buf, 'stdin') as any
+      },
     },
   }
 }
@@ -44,7 +54,7 @@ interface HLintResultItem {
 
 async function checkFile(
   buf: Atom.TextBuffer,
-  all = false,
+  mode: 'dir' | 'file' | 'stdin',
 ): Promise<undefined | UPI.IResultItem[]> {
   const bufpath = buf.getPath()
   if (!bufpath) return
@@ -52,32 +62,40 @@ async function checkFile(
     .getDirectories()
     .find((d) => d.contains(bufpath))
   const cwd = rootpath ? rootpath.getPath() : Path.dirname(bufpath)
-  const path = all ? cwd : bufpath
+  let path: string
+  if (mode === 'dir') path = cwd
+  else if (mode === 'file') path = bufpath
+  else if (mode === 'stdin') path = '-'
+  else throw new Error(`Unknown mode ${mode}`)
   try {
     const res = await new Promise<HLintResult>((resolve, reject) => {
-      CP.execFile(
+      const cp = CP.execFile(
         'hlint',
-        ['--json', '--cross', '--', path],
+        ['--json', '--cross', '--no-exit-code', '--', path],
         {
           encoding: 'utf-8',
           cwd,
           maxBuffer: Infinity,
         },
         (error, result) => {
-          if (error && result === undefined) reject(error)
+          if (error) reject(error)
           else {
             try {
               resolve(JSON.parse(result as string))
             } catch (e) {
-              reject(error)
+              reject(e)
             }
           }
         },
       )
+      if (mode === 'stdin') {
+        cp.stdin.write(buf.getText())
+        cp.stdin.end()
+      }
     })
     return Promise.all(
       res.map(async (hr) => ({
-        uri: Path.normalize(hr.file),
+        uri: Path.normalize(hr.file === '-' ? bufpath : hr.file),
         position: { row: hr.startLine - 1, column: hr.startColumn - 1 },
         message: {
           html:
@@ -99,9 +117,9 @@ async function checkFile(
     )
   } catch (e) {
     console.warn(e)
-    if (all) {
+    if (mode === 'dir') {
       try {
-        return await checkFile(buf, false)
+        return await checkFile(buf, 'file')
       } catch (e) {
         console.warn(e)
         atom.notifications.addError(e.toString(), {
